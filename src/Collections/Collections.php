7c2fa24e61be6b3159a64cddb9b25a388d56d58a
davidnsai <nsaidave@gmail.com>
2024-10-02 07:51:18 +0200

    Added a method to verify collections

---
+++ src/Collections/Collections.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lenco\Collections;
+
+use \GuzzleHttp\Client;
+
+class Collections
+{
+    // Declare private variables
+    private string $privateKey;
+    private string $publicKey;
+    protected Client $client;
+
+    // Constructor to initialize the class properties
+    public function __construct(string $privateKey)
+    {
+        $this->client = new Client();
+        $this->privateKey = $privateKey;
+    }
+
+    public function getCollection(string $paymentReference): string
+    {
+        $response = $this->client->request('GET', 'https://api.lenco.co/access/v2/collections/status/'.$paymentReference, [
+            'headers' => [
+                'Authorization' => 'Bearer '.$this->privateKey,
+                'accept' => 'application/json',
+            ],
+        ]);
+
+        return $response->getBody();
+    }
+}
